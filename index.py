ce0c50c20439814d817d9eccfac488bf37e20274
Vinlagrenouille <vincent.navales@hotmail.fr>
2017-03-13 11:00:08 -0400

    modifications de index.py : routes et arguments en url

--- index.py
+++ index.py
@@ -15,16 +15,29 @@ def show_result():
     return render_template('resultat.html')
 
 @app.route('/article/<identifiant>')
-def show_article():
-    return render_template('article.html')
+def show_article(identifiant):
+    article = get_db().get_article(identifiant)
+    if article is None:
+        return render_template('404.html'), 404
+    return render_template('article.html', article = article)
 
 @app.route('/admin')
 def show_admin_page():
-    return render_template('admin.html')
+    articles = get_db().get_articles()
+    if articles is None:
+        return render_template('accueil.html')
+    return render_template('admin.html', articles = articles)
 
 @app.route('/admin-nouveau', methods=['POST'])
 def show_new_article_page():
     return render_template('newArticle.html')
+
+@app.route('/admin-modifier/<identifiant>')
+def show_modifie_article_page(identifiant):
+    article = get_db().get_article(identifiant)
+    if article is None:
+        return render_template('404.html'), 404
+    return render_template('modifieArticle.html', article = article)
 
 @app.errorhandler(404)
 def page_not_found(e):

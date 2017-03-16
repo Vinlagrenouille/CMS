c71f39b6a0c02097670e6f38d684fe12997e2f56
Vinlagrenouille <vincent.navales@etu.u-pec.fr>
2017-03-15 23:36:05 -0400

    Delete Article.py

--- Article.py
+++
@@ -1,8 +0,0 @@
-class Article():
-    def __init__(self, id, titre, identifiant, auteur, date_publication, paragraphe):
-        self.id = id
-        self.titre = titre
-        self.identifiant = identifiant
-        self.auteur = auteur
-        self.date_publication = date_publication
-        self.paragraphe = paragraphe

ac6a0272a4cb58908720ab8da53bf48a566d011f
Vinlagrenouille <vincent.navales@hotmail.fr>
2017-03-15 22:16:49 -0400

    ajout derniers articles avec date inférieur à la date actuelle

--- Database.py
+++ Database.py
@@ -33,7 +33,7 @@ class Database:
 
     def get_article(self, identifiant):
         cursor = self.get_connection().cursor()
-        cursor.execute("select * from article where identifiant = :identifiant", {"identifiant":identifiant})
+        cursor.execute("select * from article where identifiant = :identifiant and date_publication < date('now')", {"identifiant":identifiant})
         article = cursor.fetchone()
         return article
 
